5508fd44cb755851179fca79e41e62a53c6a38d9
Ian Bobinac <ian@ianb.dev>
2025-08-20 10:38:33 +0200

    feat: add kyc-components and ToS

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import {  useState, type DOMAttributes } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+// import { TermsOfService } from "@adyen/kyc-components/experimental/react/terms-of-service";
+import "@adyen/kyc-components/experimental/terms-of-service";
 import "./App.css";
 
 function App() {
@@ -10,14 +12,42 @@ function App() {
     try {
       const response = await fetch("/.netlify/functions/token");
       console.log(response);
+      return response.json();
     } catch (error) {
       console.error("Error fetching token:", error);
     }
   };
-  fetchToken();
+
+  const legalEntityId = "LE3298G223226Q5N28LV6F6RM";
+
+  const options = {
+    locale: "en-US",
+    country: "US" as const,
+    fetchToken,
+    environment: "test",
+  };
 
   return (
     <>
+      <div>
+        ToS
+        {/* <TermsOfService legalEntityId={process.env.VITE_ADYEN_LEGALENTITYID!} options={options} /> */}
+        {/* @ts-expect-error wip */}
+        <adyen-terms-of-service
+          legalEntityId={legalEntityId}
+          options={options}
+          sdkOptions={options}
+        >
+          {/* @ts-expect-error wip */}
+        </adyen-terms-of-service>
+        {/* <adyen-terms-of-service
+          legalEntityId={process.env.VITE_ADYEN_LEGALENTITYID}
+          api-key={import.meta.env.VITE_ADYEN_LEM_API_KEY}
+          allow-origin="https://adyen-onboarding.netlify.app"
+          product="onboarding"
+          policy='{"resources":[{"legalEntityId":"{legalEntityId}","type":"legalEntity"}],"roles":["hostedOnboardingComponent"]}'
+        ></adyen-terms-of-service> */}
+      </div>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -39,3 +69,16 @@ function App() {
 }
 
 export default App;
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type CustomElement<T> = Partial<T & DOMAttributes<T> & { children?: any }>;
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace JSX {
+    interface IntrinsicElements {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      "adyen-terms-of-service": CustomElement<any>;
+    }
+  }
+}
