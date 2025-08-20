8c61ff07d1566e815bc05293e6d8aba3155875cc
Ian Bobinac <ian@ianb.dev>
2025-08-20 21:39:05 +0200

    feat: remove mpa with legacy

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { dirname, resolve } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        legacy: resolve(__dirname, "legacy/index.html"),
-      },
-    },
-  },
 });
